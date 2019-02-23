bb50460309fc135e5da9e892cbe94348552b0523
Arambula, Angel M <angel.arambula@thermofisher.com>
2019-02-22 19:46:34 -0800

    Added sample ktor rest service

---
+++ cetys-kart/src/SimpleOperation.kt
@@ -0,0 +1,14 @@
+package mx.cetys.arambula.angel
+
+data class MiObjeto(val name: String, val age: Int)
+
+fun saludarYa(): String {
+    return "saludar..........YA!"
+}
+
+fun regresarMapa(): ArrayList<MiObjeto> {
+    return arrayListOf(
+        MiObjeto("saludante", 10),
+        MiObjeto("saludante2", 10)
+    )
+}
