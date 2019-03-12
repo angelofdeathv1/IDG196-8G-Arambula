1612267798a4993032c708a967244caf8f12699c
Arambula, Angel M <angel.arambula@thermofisher.com>
2019-03-12 13:36:55 -0700

    removed config file

--- cetys-kart/src/exposed/MSSQLConfig.kt
+++
@@ -1,8 +0,0 @@
-package mx.cetys.arambula.angel.exposed
-
-
-const val EXPOSED_CONNECTION_STRING =
-    ""
-const val EXPOSED_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-const val EXPOSED_USER = ""
-const val EXPOSED_PASSWORD = ""
